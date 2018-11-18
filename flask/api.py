63ae113b720f82bd9b1f78f84e617b3356803d79
jason-sa <jandlsalazeradams@gmail.com>
2018-11-17 16:48:22 -0800

    added back spacy

--- flask/api.py
+++ flask/api.py
@@ -13,13 +13,13 @@ def load(obj_name):
     return dill.load(open(f, 'rb'))
 
 # Load the word2vec model and final classifier model
-#nlp = spacy.load('en_core_web_md')
+nlp = spacy.load('en_core_web_md')
 final_model = load('final_full_model.pkl')
 # final_model = load('final_full_model_test.pkl')
 
 # build the vocabulary of trending reviews
-#review_corpus = load('review_corpus.pkl')
-#review_vocab = [nlp.vocab[w] for w in review_corpus]
+review_corpus = load('review_corpus.pkl')
+review_vocab = [nlp.vocab[w] for w in review_corpus]
 
 
 def token_comment(comment):
